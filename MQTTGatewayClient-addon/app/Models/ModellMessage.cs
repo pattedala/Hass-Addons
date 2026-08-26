1f93379176e9e3e45f63f97b588a367a96d9f848
Patte Dala <patrik.dahlqvist@gmail.com>
2026-08-26 21:02:35 +0200

    Deleted old app - to replaced. refactored completely

--- MQTTGatewayClient-addon/app/Models/ModellMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace MQTTGatewayClient.Models
-{
-    public class ModellMessage
-    {
-        public string DeviceId { get; set; } = "";
-        public double Temperature { get; set; }
-        public DateTime Timestamp { get; set; }
-    }
-}
